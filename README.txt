2a39de9e1a4694945db504d99ce2d465708ce232
ealvar3z <55966724+ealvar3z@users.noreply.github.com>
2023-12-01 00:05:22 -0800

    Day 01

---
+++ README.txt
@@ -0,0 +1,5 @@
+Advent of Code 2021
+-------------------
+
+Usage:
+ruby dayXX.rb or ruby dayXX.rb arg1 arg2
---
+++ day01.rb
@@ -0,0 +1,27 @@
+input = File.readlines("in.txt").map(&:strip).reject(&:empty?)
+
+def part_one(lines)
+  lines.sum do |line|
+    digits = line.scan(/\d/).flatten
+    (digits.first + digits.last).to_i
+  end
+end
+
+p "1: #{part_one(input)}"
+
+DIGITS = %w(zero one two three four five six seven eight nine)
+HASHED = DIGITS.zip( (0...(DIGITS.length)).to_a.map(&:to_s) ).to_h
+
+def part_two(lines)
+  lines.sum do |line|
+    digits = line.scan(/(?=(\d|#{DIGITS.join('|')}))/).flatten
+    # digits = line.scan(/(\d|#{DIGITS.join('|')})/).flatten
+
+    first = HASHED[digits.first] || digits.first
+    last = HASHED[digits.last] || digits.last
+
+    (first + last).to_i
+  end
+end
+
+p "2: #{part_two(input)}"
